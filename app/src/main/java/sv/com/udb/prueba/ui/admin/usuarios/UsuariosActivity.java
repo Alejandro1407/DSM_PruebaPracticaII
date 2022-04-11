507f7ab961c764f1d211d92bed48be004b81d6e5
Matgaitan <matthew.ramos@novatechdev.com>
2022-04-11 15:38:48 -0600

    Some component desing and login functionality

---
+++ app/src/main/java/sv/com/udb/prueba/ui/admin/usuarios/UsuariosActivity.java
@@ -0,0 +1,16 @@
+package sv.com.udb.prueba.ui.admin.usuarios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sv.com.udb.prueba.R;
+
+public class UsuariosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuarios);
+    }
+}
