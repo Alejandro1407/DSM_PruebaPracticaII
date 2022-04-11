0fe9581aeb548e27b37235ceba316b2ea400948b
Alejandro1407 <alejandroalejo714@gmail.com>
2022-04-10 21:29:38 -0600

    Removed .idea. from git

---
+++ app/src/main/java/sv/com/udb/prueba/ui/admin/MarcasActivity.java
@@ -0,0 +1,16 @@
+package sv.com.udb.prueba.ui.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sv.com.udb.prueba.R;
+
+public class MarcasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marcas);
+    }
+}
