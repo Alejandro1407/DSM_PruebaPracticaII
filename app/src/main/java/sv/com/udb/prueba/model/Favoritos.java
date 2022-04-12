8b3eaaec23e5119db3a09c41bd9ffa0a6d1bde92
Alejandro1407 <alejandroalejo714@gmail.com>
2022-04-11 23:32:50 -0600

    [UI] - Remove lombok

--- app/src/main/java/sv/com/udb/prueba/model/Favoritos.java
+++ app/src/main/java/sv/com/udb/prueba/model/Favoritos.java
@@ -4,35 +4,85 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 import java.util.Date;
+import java.util.Objects;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@RequiredArgsConstructor
 @DatabaseTable(tableName = "favoritos_automovil")
 public class Favoritos {
 
     @DatabaseField(id = true)
     private Integer id;
 
-    @NonNull
     @DatabaseField(columnName = "fk_favoritos_automovil_usuario_idx",
             foreign = true,foreignAutoRefresh = true)
     private Usuario usuario;
 
-    @NonNull
     @DatabaseField(columnName = "fk_favoritos_automovil_automovil_idx",
             foreign = true,foreignAutoRefresh = true)
     private Automovil automovil;
 
-    @NonNull
     @DatabaseField(columnName = "fecha_agregado")
     private Date date;
+
+    public Favoritos() {
+    }
+
+    public Favoritos(Usuario usuario, Automovil automovil, Date date) {
+        this.usuario = usuario;
+        this.automovil = automovil;
+        this.date = date;
+    }
+
+    public Favoritos(Integer id, Usuario usuario, Automovil automovil, Date date) {
+        this.id = id;
+        this.usuario = usuario;
+        this.automovil = automovil;
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Automovil getAutomovil() {
+        return automovil;
+    }
+
+    public void setAutomovil(Automovil automovil) {
+        this.automovil = automovil;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Favoritos favoritos = (Favoritos) o;
+        return Objects.equals(id, favoritos.id) && Objects.equals(usuario, favoritos.usuario) && Objects.equals(automovil, favoritos.automovil) && Objects.equals(date, favoritos.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, usuario, automovil, date);
+    }
+
 
 }
--- app/src/main/java/sv/com/udb/prueba/model/Role.java
+++ app/src/main/java/sv/com/udb/prueba/model/Role.java
@@ -3,13 +3,8 @@ package sv.com.udb.prueba.model;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Objects;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @DatabaseTable(tableName = "role")
 public class Role {
 
@@ -19,4 +14,40 @@ public class Role {
     @DatabaseField(columnName = "role")
     private String role;
 
+    public Role() {
+    }
+
+    public Role(Integer id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role1 = (Role) o;
+        return Objects.equals(id, role1.id) && Objects.equals(role, role1.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, role);
+    }
 }
