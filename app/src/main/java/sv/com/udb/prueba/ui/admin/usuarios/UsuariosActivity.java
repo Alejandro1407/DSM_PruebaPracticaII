616132c1baf7ae106e5f194577fec2afe5468cc8
Matgaitan <matthew.ramos@novatechdev.com>
2022-04-11 23:32:14 -0600

    process

--- app/src/main/java/sv/com/udb/prueba/ui/admin/usuarios/UsuariosActivity.java
+++ app/src/main/java/sv/com/udb/prueba/ui/admin/usuarios/UsuariosActivity.java
@@ -10,12 +10,14 @@ import java.util.List;
 import sv.com.udb.prueba.R;
 import sv.com.udb.prueba.databinding.ActivityUsuariosBinding;
 import sv.com.udb.prueba.model.Role;
+import sv.com.udb.prueba.repositories.LoginRepositiory;
 
 public class UsuariosActivity extends AppCompatActivity {
 
     private List<Role> tipoRole = new ArrayList<>();
     private SpinnerRolAdapter spinnerRolAdapter;
     private ActivityUsuariosBinding binding;
+    private LoginRepositiory usuarioRepository;
     private Role actualRole = null;
 
     @Override
@@ -27,5 +29,7 @@ public class UsuariosActivity extends AppCompatActivity {
 
         spinnerRolAdapter = new SpinnerRolAdapter(this, android.R.layout.simple_spinner_dropdown_item,tipoRole.toArray(new Role[tipoRole.size()]));
         binding.slRol.setAdapter(spinnerRolAdapter);
+
+        usuarioRepository = new LoginRepositiory(getApplicationContext());
     }
 }
