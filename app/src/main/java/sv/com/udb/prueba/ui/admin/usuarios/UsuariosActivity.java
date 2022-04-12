276846ca034f55714ee6bd75b36249950d912592
Alejandro1407 <alejandroalejo714@gmail.com>
2022-04-11 23:31:41 -0600

    [UI] - Client UI

--- app/src/main/java/sv/com/udb/prueba/ui/admin/usuarios/UsuariosActivity.java
+++ app/src/main/java/sv/com/udb/prueba/ui/admin/usuarios/UsuariosActivity.java
@@ -1,6 +1,9 @@
 package sv.com.udb.prueba.ui.admin.usuarios;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,7 @@ import java.util.List;
 import sv.com.udb.prueba.R;
 import sv.com.udb.prueba.databinding.ActivityUsuariosBinding;
 import sv.com.udb.prueba.model.Role;
+import sv.com.udb.prueba.repositories.RoleRepository;
 
 public class UsuariosActivity extends AppCompatActivity {
 
@@ -17,6 +21,7 @@ public class UsuariosActivity extends AppCompatActivity {
     private SpinnerRolAdapter spinnerRolAdapter;
     private ActivityUsuariosBinding binding;
     private Role actualRole = null;
+    private RoleRepository roleRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +29,29 @@ public class UsuariosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_usuarios);
         binding = ActivityUsuariosBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        roleRepository = new RoleRepository(getApplicationContext());
+        init();
         spinnerRolAdapter = new SpinnerRolAdapter(this, android.R.layout.simple_spinner_dropdown_item,tipoRole.toArray(new Role[tipoRole.size()]));
         binding.slRol.setAdapter(spinnerRolAdapter);
+        binding.slRol.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                actualRole = spinnerRolAdapter.getItem(position);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //ignored
+            }
+        });
+    }
+
+    private void init(){
+        try{
+            tipoRole = roleRepository.findAll();
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(this,"Failed to recover roles",Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
