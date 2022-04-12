55a505dd68c91a936f2aa1b8252f4bbf48664bb3
Matgaitan <matthew.ramos@novatechdev.com>
2022-04-11 23:40:52 -0600

    User crud and final desing

--- app/src/main/java/sv/com/udb/prueba/ui/admin/usuarios/UsuariosActivity.java
+++ app/src/main/java/sv/com/udb/prueba/ui/admin/usuarios/UsuariosActivity.java
@@ -13,11 +13,13 @@ import java.util.List;
 import sv.com.udb.prueba.R;
 import sv.com.udb.prueba.databinding.ActivityUsuariosBinding;
 import sv.com.udb.prueba.model.Role;
+import sv.com.udb.prueba.model.Usuario;
 import sv.com.udb.prueba.repositories.LoginRepositiory;
 import sv.com.udb.prueba.repositories.RoleRepository;
 
 public class UsuariosActivity extends AppCompatActivity {
 
+    private final static String EMPTY = "";
     private List<Role> tipoRole = new ArrayList<>();
     private SpinnerRolAdapter spinnerRolAdapter;
     private ActivityUsuariosBinding binding;
@@ -37,6 +39,8 @@ public class UsuariosActivity extends AppCompatActivity {
         binding.slRol.setAdapter(spinnerRolAdapter);
 
         usuarioRepository = new LoginRepositiory(getApplicationContext());
+        binding.btnAgregar.setOnClickListener(this::onAgregarListener);
+
         binding.slRol.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -47,6 +51,40 @@ public class UsuariosActivity extends AppCompatActivity {
                 //ignored
             }
         });
+    }
+
+    private void onAgregarListener(View view){
+        String nombres;
+        String apellidos;
+        String email;
+        String user;
+        String pass;
+
+        try {
+            nombres = binding.nombres.getText().toString();
+            apellidos = binding.apellidos.getText().toString();
+            email = binding.email.getText().toString();
+            user = binding.username.getText().toString();
+            pass = binding.password.getText().toString();
+
+            if (EMPTY.equals(nombres) || EMPTY.equals(apellidos) || EMPTY.equals(email) || EMPTY.equals(user) || EMPTY.equals(pass)
+                    || actualRole == null){
+                showToast("Informacion incorrecta, no puede estar vacio");
+                return;
+            }
+
+            Usuario instance = new Usuario(null, nombres, apellidos, email, user, pass, actualRole);
+            usuarioRepository.create(instance);
+            showToast("Sucess!");
+            finish();
+        }catch (Exception e){
+            showToast("Failed to create new car");
+        }
+
+    }
+
+    private void showToast(String mesage){
+        Toast.makeText(this,mesage,Toast.LENGTH_LONG);
     }
 
     private void init(){
